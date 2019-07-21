0ba4bd0226fe858f9dc45bf6943874771329db08
Saiph1007 <48661793+Saiph1007@users.noreply.github.com>
2019-07-21 22:20:07 +0900

    Add files via upload

--- LabgeniusWeb/routes.py
+++ LabgeniusWeb/routes.py
@@ -1,14 +1,30 @@
 # routes.py
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Status
+serial_number = "MyPCR000005"
+chamber = "25.2℃"
+lid_heater = "25.2℃"
+total_time = "00:00:00"
+
+
+# default route
+# Action Table View
 @app.route('/')
-def index():
-	return render_template('./index.html', serial_number="MyPCR000005", chamber="25.2℃", lid_heater="25.2℃", total_time="00:00:00")
+def main():
+    return render_template('./main.html', serial_number=serial_number, chamber=chamber, lid_heater=lid_heater,
+                           total_time=total_time)
 
+
+# Protocol List View 수정중
+@app.route('/protocols')
+def protocols():
+    return None
+
+
+# main function
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
-
-
+    app.run(host='0.0.0.0', debug=True)
--- LabgeniusWeb/static/main.js
+++ LabgeniusWeb/static/main.js
@@ -1,44 +1,48 @@
-function ReadProtocol(){
-	var table = document.getElementById('tbody');
+// ReadProtocol Button Click Event
+function ReadProtocol() {
+    table = document.getElementById('tbody');
 
-	table.innerHTML += DefaultProtocol();
+    table.innerHTML += DefaultProtocol();
 }
 
-function DefaultProtocol () {
-	var actions = [
-	[1, 95, 30, 0],
-	[2, 95, 30, 0],
-	[3, 55, 30, 0],
-	[4, 72, 30, 0],
-	['GOTO', 2, 34, 0],
-	[5, 72, 180, 0]
-	];
-	let result = "";
-	for (let action of actions) {
-		result += '<tr>';
-		result += '<th>' + action[0] + '</th>';
-		result += '<th>' + action[1] + '</th>';
-		result += '<th>' + action[2] + '</th>';
-		result += '<th>' + action[3] + '</th>';
-		result += '</tr>';
-	}
+//make Default Protocol
+function DefaultProtocol() {
+    actions = [
+        [1, 95, 30, 0],
+        [2, 95, 30, 0],
+        [3, 55, 30, 0],
+        [4, 72, 30, 0],
+        ['GOTO', 2, 34, 0],
+        [5, 72, 180, 0]
+    ];
+    result = "";
+    for (let action of actions) {
+        result += '<tr>';
+        result += '<th>' + action[0] + '</th>';
+        result += '<th>' + action[1] + '</th>';
+        result += '<th>' + action[2] + '</th>';
+        result += '<th>' + action[3] + '</th>';
+        result += '</tr>';
+    }
 
-	return result;
+    return result;
 
 }
 
+// Start Button Click Event
 function start() {
-	var btnStart = document.getElementById('btnStart');
-	var btnStop = document.getElementById('btnStop');
-	btnStart.disabled = 'disabled';
-	btnStop.disabled = false;
-	alert('Start');
+    btnStart = document.getElementById('btnStart');
+    btnStop = document.getElementById('btnStop');
+    btnStart.disabled = 'disabled';
+    btnStop.disabled = false;
+    alert('Start');
 }
 
+//Stop Button Click Event
 function stop() {
-	var btnStart = document.getElementById('btnStart');
-	var btnStop = document.getElementById('btnStop');
-	btnStart.disabled = false;
-	btnStop.disabled = 'disabled';
-	alert('Stop');
+    btnStart = document.getElementById('btnStart');
+    btnStop = document.getElementById('btnStop');
+    btnStart.disabled = false;
+    btnStop.disabled = 'disabled';
+    alert('Stop');
 }
