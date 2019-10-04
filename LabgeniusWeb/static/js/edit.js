d12ac4bbeae8493dcedc8ce8f5f87c6846203f77
Saiph <zsewa0@gmail.com>
2019-10-04 17:22:39 +0900

    confirm change in editProtocol

--- LabgeniusWeb/static/js/edit.js
+++ LabgeniusWeb/static/js/edit.js
@@ -1,6 +1,7 @@
 var proto = null;
 var protocols = null;
 var proto_idx = null;
+var existing_text = null;
 
 // host url 
 var host = 'http://210.115.227.78:6009';
@@ -16,6 +17,7 @@ function initialized() {
             if (data.result == 'ok') {
                 protocols = data.protocols;
                 let name = document.location.href.split('/')[5];
+                console.log(document.location.href.split('/'));
                 for (let i = 0; i < protocols.length; i++) {
 
                     if (protocols[i][1] == name) {
@@ -35,37 +37,41 @@ function initialized() {
 }
 
 function loadProtocol() {
-    let value = "";
+    existing_text = "";
     let text = document.getElementById('text');
     let data = JSON.parse(proto[4]);
     proto_idx = proto[0] + "\r\n";
-    value += proto[2] + "\r\n";
+    existing_text += proto[2] + "\r\n";
     for (let i = 0; i < data.length; i++) {
         let item = data[i];
         if (item["label"] == "SHOT") {
-            value += item["label"] + "\r\n";
+            existing_text += item["label"] + "\r\n";
             continue;
         }
         if (i == data.length - 1)
-            value += item["label"] + "\t" + item["temp"] + "\t" + item["time"];
+        existing_text += item["label"] + "\t" + item["temp"] + "\t" + item["time"];
         else
-            value += item["label"] + "\t" + item["temp"] + "\t" + item["time"] + "\r\n";
+        existing_text += item["label"] + "\t" + item["temp"] + "\t" + item["time"] + "\r\n";
 
     }
-    text.value = replaceAll(value, '.0', '');
+    text.value = replaceAll(existing_text, '.0', '');
 }
 
 function editProtocol() {
-    let text = proto_idx + document.getElementById('text').value.toString();
+    let text = document.getElementById('text').value.toString();
+    if (text != existing_text) {
+        let result = confirm('Do you want to change?');
+        if (!result) return; 
+    }
     $.ajax({
         url: host + "/api/pcr/protocol/edit",
         datatype: "json",
         contentType: "text/plain",
         type: "post",
-        data: replaceAll(text, '\n', '\r\n'),
+        data: replaceAll(proto_idx + text, '\n', '\r\n'),
         success: function (data) {
             if (data.result == 'ok') {
-                history.back();
+                location.href = '/protocols';
             }
         },
         error: function (request, status, error) {
